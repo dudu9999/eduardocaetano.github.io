c90533eb57b8946a98509c4354d78eb48e16e898
Eduardo Caetano Correa <ecaetanocorrea@gmail.com>
2022-02-20 16:36:10 -0300

    Update contactus.jsx

--- src/components/contactus/contactus.jsx
+++ src/components/contactus/contactus.jsx
@@ -59,6 +59,38 @@ export default class ContactUs extends Component {
               </div>
             </aside>
           </div>
+
+          <div>
+              <!-- Messenger Plugin de bate-papo Code -->
+              <div id="fb-root"></div>
+
+              <!-- Your Plugin de bate-papo code -->
+              <div id="fb-customer-chat" class="fb-customerchat"></div>
+
+              <script>
+                var chatbox = document.getElementById('fb-customer-chat');
+                chatbox.setAttribute("page_id", "100347858431475");
+                chatbox.setAttribute("attribution", "biz_inbox");
+              </script>
+
+              <!-- Your SDK code -->
+            <script>
+              window.fbAsyncInit = function() {
+                FB.init({
+                  xfbml            : true,
+                  version          : 'v13.0'
+                });
+              };
+
+              (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/pt_BR/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));
+            </script>                    
+          </div>
         </section> {/* Contact Section End*/}
       </React.Fragment>
     );
